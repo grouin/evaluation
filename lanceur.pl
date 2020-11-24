c2142f8e1bb80b69c5a1738611f7608dfa948ecd
Cyril Grouin <cyril.grouin@limsi.fr>
2020-11-24 10:38:36 +0100

    Commentaires et nettoyage des lanceurs

--- lanceur.pl
+++ lanceur.pl
@@ -1,17 +1,29 @@
 #!/usr/bin/perl
 
-# Un répertoire contient les textes, plusieurs répertoires contiennent
-# les annotations au format BRAT. Le script aligne les différentes
-# versions annotées.
+# Un répertoire contient des fichiers textes (ref/*txt), plusieurs
+# répertoires contiennent les annotations correspondant aux précédents
+# fichiers (sys1/ sys2/ sys3/), au format BRAT (*ann).
+# Chaque soumission d'un même système d'une même équipe (autorisation
+# de trois soumissions par équipe) est dans un répertoire différent.
+# Le ROVER est appliqué sur toutes les soumissions reçues, quel que
+# soit le nombre de soumissions par équipe.
+
+# Ce lanceur applique les différents scripts : alignement des versions
+# annotées des fichiers *txt, application du ROVER, et conversion des
+# annotations majoritaires au format BRAT en enregistrant les sorties
+# *ann générées dans le répertoire des fichiers textes (ref/).
 
 # perl lanceur.pl ref/ sys1/ sys2/ sys3/
+
+# Auteur : Cyril Grouin, novembre 2020.
+
 
 use strict;
 
 my @chemins=@ARGV;
 
 # Solution simple : le premier dossier contient les textes, tous les
-# autres contiennent les annotations
+# répertoires suivants contiennent les annotations à aligner
 
 my @textes=<$chemins[0]/*txt>;
 
--- lanceur.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/bash
-
-perl outputs-alignment.pl ref/363052.txt sys1/363052.ann sys2/363052.ann sys3/363052.ann | perl rover-production.pl | perl brat-conversion.pl >ref/363052.ann
